c137c8c95829f92f4e56ccb147b32f2a0b616f31
Roderick Hsiao <roderickhsiao@gmail.com>
2021-11-22 19:05:17 -0800

    Add file dependency when in watch mode

--- lib/atomicLoader.ts
+++ lib/atomicLoader.ts
@@ -103,13 +103,16 @@ const atomicLoader = function (source, map) {
 
     const query = getOptions(this) || {};
     const { config, configPath = [], minimize = false, postcssPlugins = [] } = query;
-    let validPostcssPlugins = DEFAULT_POSTCSS_PLUGIN_LIST;
+    const validPostcssPlugins = Array.isArray(postcssPlugins) ? postcssPlugins : DEFAULT_POSTCSS_PLUGIN_LIST;
+    const addDependency = this.addDependency;
 
-    if (Array.isArray(postcssPlugins)) {
-        validPostcssPlugins = postcssPlugins;
-    }
     let configs = [
-        ...[].concat(configPath).map((configPath) => require(require.resolve(configPath))),
+        ...[].concat(configPath).map((configPath) => {
+            if (addDependency) {
+                addDependency(configPath);
+            }
+            return require(require.resolve(configPath))
+        }),
         ...(config !== undefined ? [config] : []),
     ];
 
